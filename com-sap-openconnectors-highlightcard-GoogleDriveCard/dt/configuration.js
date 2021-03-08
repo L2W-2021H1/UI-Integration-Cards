8561c502990768f60b69e2a08567d6d9d2c27652
Mat Beerkens <mat.beerkens@gmail.com>
2021-03-08 09:17:09 +0100

    reamde

---
+++ com-sap-openconnectors-highlightcard-GoogleDriveCard/dt/configuration.js
@@ -0,0 +1,24 @@
+/*!
+ * OpenUI5
+ * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
+ * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
+ */
+
+/**
+ * This module was created by the BASEditor
+ */
+sap.ui.define(["sap/ui/integration/Designtime"], function (
+    Designtime
+) {
+    "use strict";
+    return function () {
+        return new Designtime({
+	"form": {
+		"items": {}
+	},
+	"preview": {
+		"modes": "Abstract"
+	}
+});
+    };
+});
